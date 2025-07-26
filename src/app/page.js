4edd3eb8673ae590330a99f58f03056857bc91ed
Mohammed Slatnia <slatnia.mmed@gmail.com>
2025-07-26 18:12:48 +0100

    Final fixes

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import JobSection from "@/components/JobSection";
 import QuoteSection from "@/components/QuoteSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import CredibilitySection from "@/components/CredibilitySection";
+import CollaborationSection from "@/components/CollaborationSection";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <QuoteSection />
       <ProjectsSection />
       <CredibilitySection />
+      <CollaborationSection />
     </>
   );
 }
---
+++ src/components/CollaborationSection.jsx
@@ -0,0 +1,46 @@
+import "./CollaborationSection.css";
+
+export default function CollaborationSection() {
+  return (
+    <section className="CollaborationSection">
+      {/* === TITLE + LOGOS === */}
+      <div className="companiesBlock">
+        <h4>Worked and collaborated with world-class companies</h4>
+        <div className="logos">
+          <img src="images/logos/logo1.png" alt="Company 1" />
+          <img src="images/logos/logo2.png" alt="Company 2" />
+          <img src="images/logos/logo3.png" alt="Company 3" />
+          <img src="images/logos/logo4.png" alt="Company 4" />
+          <img src="images/logos/logo5.png" alt="Company 5" />
+          <img src="images/logos/logo6.png" alt="Company 6" />
+          <img src="images/logos/logo7.png" alt="Company 7" />
+        </div>
+      </div>
+
+      {/* === NEWSLETTER + FOOTER === */}
+      <div className="newsletterFooter">
+        <div className="newsletter">
+          <h5>Subscribe newsletter for interesting information</h5>
+          <form>
+            <input
+              type="email"
+              placeholder="Enter your email to subscribe ..."
+            />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
+
+        <div className="footerLinks">
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact</a>
+          </nav>
+          <p>© Copyright by AltDesain-Studio – All right reserved.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
